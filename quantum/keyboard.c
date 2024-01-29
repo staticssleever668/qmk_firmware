1497505fdf483ec5ddf88af662fb2b7ef81dcb2b
Yaroslav Chvanov <yaroslav.chvanov@gmail.com>
2024-01-30 00:48:17 +0300

    attempt another, including dbg logs

--- quantum/keyboard.c
+++ quantum/keyboard.c
@@ -32,6 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "util.h"
 #include "sendchar.h"
 #include "eeconfig.h"
+#include "action_util.h"
 #include "action_layer.h"
 #ifdef AUDIO_ENABLE
 #    include "audio.h"
@@ -517,6 +518,10 @@ static bool matrix_task(void) {
 
     matrix_scan_perf_task();
 
+    if (should_send_report()) {
+        do_send_keyboard_report();
+    }
+
     // Short-circuit the complete matrix processing if it is not necessary
     if (!matrix_changed) {
         generate_tick_event();
@@ -551,6 +556,10 @@ static bool matrix_task(void) {
         }
 
         matrix_previous[row] = current_row;
+    }
+
+    if (should_send_report()) {
+        do_send_keyboard_report();
     }
 
     return matrix_changed;
